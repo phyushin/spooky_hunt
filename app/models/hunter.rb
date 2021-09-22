4fd9a7b89a59af59cc0d2b08f1d852ae292820f9
phyushin <phyushin@gmail.com>
2021-09-22 22:25:07 +0100

    TwEaKs

--- app/models/hunter.rb
+++ app/models/hunter.rb
@@ -1,2 +1,9 @@
 class Hunter < ApplicationRecord
+  before_save :generate_hunter_uuid
+
+  private
+
+  def generate_hunter_uuid
+    self.hunter_uuid = SecureRandom.uuid
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_152203) do
+ActiveRecord::Schema.define(version: 2021_09_21_121057) do
 
   create_table "ghosts", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_09_20_152203) do
     t.string "hunter_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "hunter_uuid"
   end
 
 end
